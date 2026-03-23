d780c065df261603710afe268777df12e49a9304
Nils Lockean <nils.lockean@texthelp.com>
2026-03-23 10:29:16 +0100

    Add appId to deployment and fix api trailing comma

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,7 +3,7 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'dajcvves',
-    dataset: 'production'
+    dataset: 'production',
   },
   deployment: {
     /**
@@ -11,5 +11,6 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
-  }
+    appId: 'i6bi3p2syxwa7r353arlmwa9',
+  },
 })
